54e517b8b44c7164ff99776ebad5805cf97ed5fc
Deepthiabbaraju <112412019+Deepthiabbaraju@users.noreply.github.com>
2022-09-11 12:50:44 +0530

    Update ElectricityBillTest.java

--- ElectricityBillTest.java
+++ ElectricityBillTest.java
@@ -3,19 +3,19 @@ public class ElectricityBillTest{
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         ElectricityBill eb=new ElectricityBill();
-        System.out.print("Enter consumer number:");
+        System.out.println("Enter consumer number:");
         eb.cnum=sc.next();
-        System.out.print("enter consumer name:");
+        System.out.println("enter consumer name:");
         eb.cname=sc.next();
-        System.out.print("enter previous month readings");
+        System.out.println("enter previous month readings");
         eb.pmr=sc.nextInt();
         System.out.println("enter current month readingsr");
         eb.cmr=sc.nextInt();
-        System.out.print("enter 1 if eb connection is domestic,2 if commercial");
+        System.out.println("enter 1 if eb connection is domestic,2 if commercial");
         eb.type=sc.nextInt();
         System.out.println("consumer name"+eb.cname());
         System.out.println("consumer numbrr"+eb.cnum());
-        System.out.print(eb.amountcal());
+        System.out.println(eb.amountcal());
     }
 }
 
