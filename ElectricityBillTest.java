3c902edc9b5e0e2862dfd78e5dd15564afd89114
Deepthiabbaraju <112412019+Deepthiabbaraju@users.noreply.github.com>
2022-09-11 12:50:06 +0530

    Update ElectricityBillTest.java

--- ElectricityBillTest.java
+++ ElectricityBillTest.java
@@ -9,10 +9,12 @@ public class ElectricityBillTest{
         eb.cname=sc.next();
         System.out.print("enter previous month readings");
         eb.pmr=sc.nextInt();
-        System.out.println("ener cmr");
+        System.out.println("enter current month readingsr");
         eb.cmr=sc.nextInt();
-        System.out.print("enter type");
+        System.out.print("enter 1 if eb connection is domestic,2 if commercial");
         eb.type=sc.nextInt();
+        System.out.println("consumer name"+eb.cname());
+        System.out.println("consumer numbrr"+eb.cnum());
         System.out.print(eb.amountcal());
     }
 }
